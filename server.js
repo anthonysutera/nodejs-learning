48f411aad1955118ea2325cc93e2c0ef051ba9b5
anthony.sutera <anthony.sutera@accenture.com>
2018-04-03 11:20:21 +0200

    Add start on the script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -23,9 +24,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.render('maintenance.hbs');
-});
+}); */
 
 app.use(express.static(__dirname + '/public'));
 
@@ -58,4 +59,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(port ,() => {
+    console.log(`Server is running on port ${port}`);
+});
